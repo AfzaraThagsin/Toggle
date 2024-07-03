970b38d26a665eec44158e584f19ae5238cb7716
AFZARATHAGSIN <afzarashajahan@gmail.com>
2024-07-03 08:04:41 +0530

    first commit

--- src/components/ToggleEyeComponents/ToggleEyeComponent.jsx
+++ src/components/ToggleEyeComponents/ToggleEyeComponent.jsx
@@ -1,80 +1,3 @@
-// // import React from 'react'
-// // import './ToggleEyeComponent.css'
-// // function ToggleEyeComponent() {
-    
-// //   return (
-// //        <div className='contain'>
-// //         <div className="container">
-// //             <img src="https://www.svgrepo.com/show/12044/eye.svg" alt="Eye Logo"></img>
-// //             <h2>Name: AFZARA THAGSIN</h2>
-// //             <h3>Registration No: 123456</h3>
-// //             <div className="button">
-// //                 <button className="click " onClick>Show Details</button>
-// //                  <p >Additional details or content can go here...</p> 
-         
-// //             </div>
-// //         </div>
-// //         </div>
-       
-// //   )
-// // }
-
-// // export default ToggleEyeComponent
-
-// import React, { useState } from 'react';
-// import './ToggleEyeComponent.css';
-
-// function ToggleEyeComponent() {
-//     const [showDetails, setShowDetails] = useState(false);
-
-//     const toggleDetails = () => {
-//         setShowDetails(!showDetails);
-//     }
-
-//     return (
-//         // <div className='contain'>
-//             <div className="container">
-//                 <img src="https://www.svgrepo.com/show/12044/eye.svg" alt="Eye Logo"></img>
-//                 <h2>Name: AFZARA THAGSIN</h2>
-//                 <h3>Registration No: 123456</h3>
-//                 <div className="button">
-//                     <button className="click" onClick={toggleDetails}>
-//                         {showDetails ? "Hide Details" : "Show Details"}
-//                     </button>
-//                 </div>
-//                 {showDetails && (
-//                     <div className="para">
-//                         <p>Additional details or content can go here...</p>
-//                     </div>
-//                 )}
-//             </div>
-//         // </div>
-//     );
-// }
-
-// export default ToggleEyeComponent;
-// import React from 'react'
-// import './ToggleEyeComponent.css'
-// function ToggleEyeComponent() {
-    
-//   return (
-//        <div className='contain'>
-//         <div className="container">
-//             <img src="https://www.svgrepo.com/show/12044/eye.svg" alt="Eye Logo"></img>
-//             <h2>Name: AFZARA THAGSIN</h2>
-//             <h3>Registration No: 123456</h3>
-//             <div className="button">
-//                 <button className="click " onClick>Show Details</button>
-//                  <p >Additional details or content can go here...</p> 
-         
-//             </div>
-//         </div>
-//         </div>
-       
-//   )
-// }
-
-// export default ToggleEyeComponent
 
 import React, { useState } from 'react';
 import './ToggleEyeComponent.css';
@@ -87,7 +10,7 @@ function ToggleEyeComponent() {
     }
 
     return (
-        // <div className='contain'>
+      
             <div className="container">
                 <img src="https://www.svgrepo.com/show/12044/eye.svg" alt="Eye Logo"></img>
                 <h2>Name: AFZARA THAGSIN</h2>
